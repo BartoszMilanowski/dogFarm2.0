0f589982ca4d148c05fd93300121c8f3aaa27a86
BartoszMilanowski <bartosz.milanowski@protonmail.com>
2023-10-24 18:55:37 +0200

    import dog.html from static ver and change it to dog.php

---
+++ dog.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="utf-8">
+    <script async src="https://kit.fontawesome.com/6cc05e1e8e.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/style.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <title> Labrador Retriever - Z Krainy Narwi demo</title>
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav class="navbar">
+        <a href="index.html">
+            <img class="navbar-logo" src="/images/logo.png" />
+        </a>
+        <ul class="menu-list">
+            <i class="fas fa-times close-menu"></i>
+            <li><a class="menu-link" href="/about.html">O nas</a></li>
+            <li><a class="menu-link" href="/breed.html">Labrador Retriever</a></li>
+            <li><a class="menu-link" href="#">Bichon Frise</a></li>
+            <li><a class="menu-link" href="#">Lagotto Romangolo</a></li>
+            <li><a class="menu-link" href="#">Chiuahua</a></li>
+            <li><a class="menu-link" href="/index.html#contact">Kontakt</a></li>
+        </ul>
+        <i class="fas fa-bars burger-icon"></i>
+    </nav>
+    <section id="about-dog">
+        <div class="about-dog">
+            <div class="about-dog-img">
+                <img src="/images/dog1.jpg" />
+            </div>
+            <div class="about-dog-text">
+                <span class="dog-name">Hiro-Haruko Zandalle</span>
+                <span class="by-judges-title">W oczach sędziów</span>
+                <p class="by-judges">Głowa doskonała, o szlachetnym wyrazie, proporcjonalnie zbudowany, prawidłowe kątowanie kończyn,
+                    dobra pigmentacja oka, dobrze osadzony ogon, doskonały w ruchu.</p>
+            </div>
+        </div>
+    </section>
+    <!--Gallery-->
+    <section id="about-gallery">
+        <div class="gallery">
+            <div class="gallery-item">
+                <img src="/images/labrador-main.jpg" />
+            </div>
+            <div class="gallery-item">
+                <img src="/images/lagotto-main.jpg" />
+            </div>
+            <div class="gallery-item">
+                <img src="/images/labrador-chocolate.jpg" />
+            </div>
+            <div class="gallery-item">
+                <img src="/images/labrador2.jpg" />
+            </div>
+            <div class="gallery-item">
+                <img src="/images/bichon-main.jpg" />
+            </div>
+            <div class="gallery-item">
+                <img src="/images/chiuahua-main.jpg" />
+            </div>
+        </div>
+    </section>
+     <!--Contact-->
+     <section id="contact">
+        <h2>Kontakt</h2>
+        <div class="contact-data">
+            <span>Jan Kowalski</span>
+            <span><a href='tel:+48123456789'>123-456-789</a></span>
+            <span>Główna 10, 00-000 Marki</span>
+            <span><a href="mailto:info@zkrainynarwi.pl">info@zkrainynarwi.pl</a></span>
+            <span><a href='#'><img class="sm-icon" src="/images/facebook.png"></a></span>
+        </div>
+    </section>
+    <!-- Footer-->
+    <section id="footer">
+        <span>&#169;2023 Z Krainy Narwi, wykonanie: Bartosz Milanowski</span>
+    </section>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
