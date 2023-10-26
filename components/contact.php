574c1dbd9c094e2fdc64d2d37911d0980737d641
BartoszMilanowski <bartosz.milanowski@protonmail.com>
2023-10-26 17:08:18 +0200

    change fetch method in contact.php

--- components/contact.php
+++ components/contact.php
@@ -2,19 +2,23 @@
 
 require_once 'configure/db_connect.php';
 
-$contactQuery = "SELECT contact_data FROM contact WHERE id = 1";
+// $contactQuery = "SELECT contact_data FROM contact WHERE id = 1";
 
-$result = $db->query($contactQuery);
-if ($result) {
-    $row = $result->fetch();
-    if ($row) {
-        $contactData = json_decode($row["contact_data"], true);
-    }
-}
+// $result = $db->query($contactQuery);
+// if ($result) {
+//     $row = $result->fetch();
+//     if ($row) {
+//         $contactData = json_decode($row["contact_data"], true);
+//     }
+// }
 
-$link_tel = "tel:" . $contactData["phone"];
-$link_mail = "mailto:" . $contactData["email"];
-$link_fb = $contactData['fb_link'];
+$contactQuery  =$db->prepare('SELECT * FROM contact WHERE Id = 1');
+$contactQuery->execute();
+$contact = $contactQuery->fetch(PDO::FETCH_ASSOC);
+
+$link_tel = "tel:" . $contact["phone"];
+$link_mail = "mailto:" . $contact["email"];
+$link_fb = $contact['fb_link'];
 
 
 ?>
@@ -26,17 +30,17 @@ $link_fb = $contactData['fb_link'];
     </h2>
     <div class="contact-data">
         <span>
-            <?= "{$contactData['name']}" ?>,
+            <?= "{$contact['name']}" ?>,
         </span>
         <span><a href="<?= $link_tel ?>">
-                <?= "{$contactData['phone']}" ?>,
+                <?= "{$contact['phone']}" ?>,
             </a></span>
         <span>
-            <?= "{$contactData['address']}" ?>,
+            <?= "{$contact['address']}" ?>,
             <?= $_SESSION['lang'] == 'pl' ? '' : "Poland,"?>
         </span>
         <span><a href="<?= $link_mail ?>">
-                <?= "{$contactData['email']}" ?>
+                <?= "{$contact['email']}" ?>
             </a></span>
         <span><a href="<?= $link_fb ?>" target="_blank"><img class="sm-icon" src="images/icons/facebook.png"></a></span>
     </div>
